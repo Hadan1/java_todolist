320cd5a5c8b6801ab41bcd42128c7b9f6882bc64
Dmitry <reita142@ya.ru>
2025-07-15 04:03:07 +0300

    gitignore updated

--- src/main/java/app/todo/controller/UserController.java
+++ src/main/java/app/todo/controller/UserController.java
@@ -3,11 +3,13 @@ package app.todo.controller;
 import app.todo.repository.User;
 import app.todo.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "api/users")
 public class UserController {
 
     private final UserService userService;
